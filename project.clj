97c361161c5683b2cea8fbf9fac9ae83d9467bc1
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-07-13 09:15:45 -0600

    make lein-compile work
    Signed-off-by: Deepak Giridharagopal <deepak@puppetlabs.com>

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject icfp "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+
+  :aot [icfp.core]
+  :main icfp.core
+)
