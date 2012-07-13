224f09abd31ab51f9882a4a71cb79d3776765a01
Chris Price <chris@puppetlabs.com>
2012-07-13 15:05:34 -0700

    Successfully constructing game-state from map/files
    still missing some of the member values (lambdas, robot, rocks)

--- src/chris_scratch/readmap.clj
+++ src/chris_scratch/readmap.clj
@@ -1,8 +1,19 @@
-(ns icfp.chris_scratch.readmap)
+(ns icfp.chris_scratch.readmap
+  (:require icfp.core))
+
+(def valid-map-chars #{"#" "\\" "." " " "R" "L" "*"})
+
+(defn is-valid-map-char? [c]
+  (let [valid (not (nil? (valid-map-chars c)))]
+    (if-not valid (println (format "Invalid map char '%s'" c)))
+    valid))
 
 (defn char-to-keyword [c]
+  {  :pre [(is-valid-map-char? c)] }
   (keyword
     (condp = c
+      ; convert a few of the input symbols to things that are easier to
+      ;  represent as literals in clojure
       " " "_"
       "\\" ">"
       c)))
@@ -10,20 +21,61 @@
 (defn line-to-keyword-seq [line]
   (map char-to-keyword (map str line)))
 
-(defn read-nested-seqs [rdr]
-    (map line-to-keyword-seq (clojure.string/split-lines (slurp rdr))))
+;(defstruct map-file-lines :map-lines :metadata-lines)
+
+(defn find-empty-line-index [lines]
+  (let [filtered (first (filter #(re-matches #"^\s*$" (second %)) (map-indexed vector lines)))]
+    (if (empty? filtered)
+      (count lines)
+      (first filtered))))
+
+(defn get-map-file-lines [rdr]
+  (let [lines (clojure.string/split-lines (slurp rdr))
+       empty-line-index (find-empty-line-index lines)]
+    {
+      :map-lines (subvec lines 0 empty-line-index)
+      :metadata-lines (if (= (count lines) empty-line-index)
+                        []
+                        (subvec lines (+ 1 empty-line-index) (count lines)))
+    }))
 
 (defn line-seq-to-hash [line]
   (into {} (map-indexed #(vec [(+ 1 %1) %2]) line)))
 
-(defn read-map [rdr]
-  (let [myarr (read-nested-seqs rdr)]
-    (into {} (map-indexed #(vec [(- (count myarr) %1) (line-seq-to-hash %2)]) myarr))))
+(defn split-metadata-line [line]
+  (clojure.string/split line #"\s+"))
 
-(with-open
-  [rdr (clojure.java.io/reader *in*)]
-  (println (read-map rdr)))
-;
-;(with-open
-;  [rdr (clojure.java.io/reader "/home/cprice/work/puppet/icfp/icfp-2012/resources/maps/map1.txt")]
-;  (println (read-map rdr)))
+(defn metadata-lines-to-map [lines]
+  (into {} (map #(vec (split-metadata-line %1)) lines)))
+
+(defn read-game-state [rdr]
+  (let [map-file-lines (get-map-file-lines rdr)
+        rows (map line-to-keyword-seq (:map-lines map-file-lines))
+        numrows (count rows)
+        metadata (metadata-lines-to-map (:metadata-lines map-file-lines))]
+    (let [board (into {}
+                  (map-indexed
+                    #(vec [(- numrows %1) (line-seq-to-hash %2)])
+                    rows))
+         water (metadata "Water")
+         flooding (metadata "Flooding")
+         waterproof (metadata "Waterproof")
+         ]
+      (struct-map icfp.core/game-state
+        :board board
+        :water water
+        :flooding flooding
+        :waterproof waterproof
+        :score 0
+        )
+    )))
+
+
+(defn read-game-state-from-file [path]
+  (with-open
+    [rdr (clojure.java.io/reader path)]
+    (println (read-game-state rdr))))
+
+;(read-map-from-file *in*)
+(read-game-state-from-file "/home/cprice/work/puppet/icfp/icfp-2012/resources/maps/contest1.map")
+(read-game-state-from-file "/home/cprice/work/puppet/icfp/icfp-2012/resources/maps/flood5.map")
--- src/icfp/core.clj
+++ src/icfp/core.clj
@@ -1,155 +1,156 @@
 (ns icfp.core)
 
-(defstruct game-state :board :lambdas :rocks :robot :score :moves)
-
-(defn robot?
-  [{:keys [board]} position]
-  (= (get-in board position) :R))
-
-(defn rock?
-  [{:keys [board]} position]
-  (= (get-in board position) :*))
-
-(defn space?
-  [{:keys [board]} position]
-  (= (get-in board position) :_))
-
-(defn wall?
-  [{:keys [board]} position]
-  (= (get-in board position) :#))
-
-(defn earth?
-  [{:keys [board]} position]
-  (= (get-in board position) :.))
-
-(defn lambda?
-  [{:keys [board]} position]
-  (= (get-in board position) :>))
-
-(defn lift?
-  [{:keys [board]} position]
-  (= (get-in board position) :L))
-
-(defn open-lift?
-  [{:keys [board lambdas] :as game-state} position]
-  (and (empty? lambdas)
-       (lift? board position)))
-
-(defn width
-  [board]
-  (count board))
-
-(defn height
-  [board]
-  (count (board 1)))
-
-(defn left
-  [position]
-  [(dec (first position)) (last position)])
-
-(def lleft
-  (comp left left))
-
-(defn right
-  [position]
-  [(inc (first position)) (last position)])
-
-(def rright
-  (comp right right))
-
-(defn up
-  [position]
-  [(first position) (inc (last position))])
-
-(defn down
-  [position]
-  [(first position) (dec (last position))])
-
-(def movable?
-  (some-fn space? earth? open-lift? lambda?))
-
-(defn move-allowed?
-  [{:keys [board robot-position] :as game-state} move]
-  (condp = move
-    :U (movable? game-state (up robot-position))
-    :D (movable? game-state (down robot-position))
-    :L (or (movable? game-state (left robot-position))
-           (and (rock? game-state (left robot-position))
-                 (space? game-state (lleft robot-position))))
-    :R (or (movable? game-state (right robot-position))
-           (and (rock? game-state (right robot-position))
-                 (space? game-state (rright robot-position))))))
-
-(defn command-allowed?
-  [{:keys [board robot-position] :as game-state} command]
-  (condp = command
-    :L (move-allowed? board :L)
-    :R (move-allowed? board :R)
-    :U (move-allowed? board :U)
-    :D (move-allowed? board :D)
-    :W true
-    :A true))
-
-(defn move-sideways
-  [{:keys [board robot] :as game-state} direction]
-  ;; We already know the move is legal, so it's either a rock or not. If it's a
-  ;; rock, move the robot, remove the old rock, add the new rock. Otherwise
-  ;; just move the robot.
-  (if (rock? game-state (direction robot))
-    (-> game-state
-      (update-in [:robot] direction)
-      (update-in [:rocks] disj (direction robot))
-      (update-in [:rocks] conj (direction (direction robot))))
-    (update-in [:robot] direction)))
-
-(defn move
-  [{:keys [board robot] :as game-state} command]
-  (condp = command
-    :L (move-sideways game-state left)
-    :R (move-sideways game-state right)
-    :U (update-in game-state [:robot] up)
-    :D (update-in game-state [:robot] down)))
-
-(defn execute-command
-  [{:keys [moves] :as game-state} command]
-  (if (command-allowed? game-state command)
-    (cond
-      ([:L :R :U :D] command)
-      (move game-state command)
-
-      ([:W :A] command)
-      nil)
-    (update-in game-state [:moves] conj command)
-    (execute-command game-state :W)))
-
-(defn fall-down
-  [board position]
-  (let [lower-pos (down position)]
-    (if ((some-fn space? robot?) lower-pos)
-      lower-pos)))
-
-(defn fall-right
-  [board position]
-  (let [lower-pos (down position)
-        right-pos (right position)
-        lower-right-pos (down (right position))]
-    (if (and ((some-fn rock? lambda?) lower-pos)
-             (space? right-pos)
-             (space? lower-right-pos)))))
-
-(defn fall-left
-  [board position]
-  (let [lower-pos (down position)
-        left-pos (left position)
-        lower-left-pos (down (left position))]
-    (if (and (rock? lower-pos)
-             (space? left-pos)
-             (space? lower-left-pos)))))
-
-(def fall-rock
-  (some-fn fall-down fall-right fall-left last))
-
-(defn update-board
-  [{:keys [board rocks] :as game-state}]
-  (let [new-rocks (set (map (partial fall-rock board) rocks))]
-    (assoc game-state :rocks new-rocks)))
+(defstruct game-state :board :lambdas :rocks :robot :score :moves
+    :water :flooding :waterproof)
+;
+;(defn robot?
+;  [{:keys [board]} position]
+;  (= (get-in board position) :R))
+;
+;(defn rock?
+;  [{:keys [board]} position]
+;  (= (get-in board position) :*))
+;
+;(defn space?
+;  [{:keys [board]} position]
+;  (= (get-in board position) :_))
+;
+;(defn wall?
+;  [{:keys [board]} position]
+;  (= (get-in board position) :#))
+;
+;(defn earth?
+;  [{:keys [board]} position]
+;  (= (get-in board position) :.))
+;
+;(defn lambda?
+;  [{:keys [board]} position]
+;  (= (get-in board position) :>))
+;
+;(defn lift?
+;  [{:keys [board]} position]
+;  (= (get-in board position) :L))
+;
+;(defn open-lift?
+;  [{:keys [board lambdas] :as game-state} position]
+;  (and (empty? lambdas)
+;       (lift? board position)))
+;
+;(defn width
+;  [board]
+;  (count board))
+;
+;(defn height
+;  [board]
+;  (count (board 1)))
+;
+;(defn left
+;  [position]
+;  [(dec (first position)) (last position)])
+;
+;(def lleft
+;  (comp left left))
+;
+;(defn right
+;  [position]
+;  [(inc (first position)) (last position)])
+;
+;(def rright
+;  (comp right right))
+;
+;(defn up
+;  [position]
+;  [(first position) (inc (last position))])
+;
+;(defn down
+;  [position]
+;  [(first position) (dec (last position))])
+;
+;(def movable?
+;  (some-fn space? earth? open-lift? lambda?))
+;
+;(defn move-allowed?
+;  [{:keys [board robot-position] :as game-state} move]
+;  (condp = move
+;    :U (movable? game-state (up robot-position))
+;    :D (movable? game-state (down robot-position))
+;    :L (or (movable? game-state (left robot-position))
+;           (and (rock? game-state (left robot-position))
+;                 (space? game-state (lleft robot-position))))
+;    :R (or (movable? game-state (right robot-position))
+;           (and (rock? game-state (right robot-position))
+;                 (space? game-state (rright robot-position))))))
+;
+;(defn command-allowed?
+;  [{:keys [board robot-position] :as game-state} command]
+;  (condp = command
+;    :L (move-allowed? board :L)
+;    :R (move-allowed? board :R)
+;    :U (move-allowed? board :U)
+;    :D (move-allowed? board :D)
+;    :W true
+;    :A true))
+;
+;(defn move-sideways
+;  [{:keys [board robot] :as game-state} direction]
+;  ;; We already know the move is legal, so it's either a rock or not. If it's a
+;  ;; rock, move the robot, remove the old rock, add the new rock. Otherwise
+;  ;; just move the robot.
+;  (if (rock? game-state (direction robot))
+;    (-> game-state
+;      (update-in [:robot] direction)
+;      (update-in [:rocks] disj (direction robot))
+;      (update-in [:rocks] conj (direction (direction robot))))
+;    (update-in [:robot] direction)))
+;
+;(defn move
+;  [{:keys [board robot] :as game-state} command]
+;  (condp = command
+;    :L (move-sideways game-state left)
+;    :R (move-sideways game-state right)
+;    :U (update-in game-state [:robot] up)
+;    :D (update-in game-state [:robot] down)))
+;
+;(defn execute-command
+;  [{:keys [moves] :as game-state} command]
+;  (if (command-allowed? game-state command)
+;    (cond
+;      ([:L :R :U :D] command)
+;      (move game-state command)
+;
+;      ([:W :A] command)
+;      nil)
+;    (update-in game-state [:moves] conj command)
+;    (execute-command game-state :W)))
+;
+;(defn fall-down
+;  [board position]
+;  (let [lower-pos (down position)]
+;    (if ((some-fn space? robot?) lower-pos)
+;      lower-pos)))
+;
+;(defn fall-right
+;  [board position]
+;  (let [lower-pos (down position)
+;        right-pos (right position)
+;        lower-right-pos (down (right position))]
+;    (if (and ((some-fn rock? lambda?) lower-pos)
+;             (space? right-pos)
+;             (space? lower-right-pos)))))
+;
+;(defn fall-left
+;  [board position]
+;  (let [lower-pos (down position)
+;        left-pos (left position)
+;        lower-left-pos (down (left position))]
+;    (if (and (rock? lower-pos)
+;             (space? left-pos)
+;             (space? lower-left-pos)))))
+;
+;(def fall-rock
+;  (some-fn fall-down fall-right fall-left last))
+;
+;(defn update-board
+;  [{:keys [board rocks] :as game-state}]
+;  (let [new-rocks (set (map (partial fall-rock board) rocks))]
+;    (assoc game-state :rocks new-rocks)))
