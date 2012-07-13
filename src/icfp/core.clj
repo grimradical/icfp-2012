b91cc6868b8ca1a551c7accb8aaefe95bc63b558
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-07-13 09:01:30 -0600

    project skeleton
    Signed-off-by: Deepak Giridharagopal <deepak@puppetlabs.com>

---
+++ src/icfp/core.clj
@@ -0,0 +1,1 @@
+(ns icfp.core)
---
+++ test/icfp/test/core.clj
@@ -0,0 +1,6 @@
+(ns icfp.test.core
+  (:use [icfp.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
