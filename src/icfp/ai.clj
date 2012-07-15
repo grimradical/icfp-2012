28d9d0639ab288a4f61461f6fc3cf4324ceddec3
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-07-15 12:33:43 -0600

    simple, cost-based ia

--- src/icfp/ai.clj
+++ src/icfp/ai.clj
@@ -110,63 +110,48 @@
 
 (defn closest
   [pos candidates]
-  {:pre  [candidates]
-   :post [(some #{%} candidates)]}
-  (let [dists    (into {} (for [c candidates]
-                                   [(manhattan-distance pos c) c]))
-        min-dist (apply min (keys dists))]
-    (dists min-dist)))
+  {:pre  [candidates]}
+  (let [dists        (for [c candidates]
+                       [(manhattan-distance pos c) c])
+        dists        (sort dists)
+        [min-dist _] (first dists)]
+    (map second
+         (filter #(= (first %) min-dist) dists))))
 
-
-(defn valid-dest?
-  [{:keys [lambdas lift] :as game-state} pos]
-  (or (some #{pos} lambdas)
-      (= lift pos)))
-
-(defn stupid-1
-  [{:keys [robot lambdas lift moves visited prev-dest] :as game-state :or {visited []}}]
-  (let [dest            (if (valid-dest? game-state prev-dest)
-                          prev-dest
-                          (if (empty? lambdas)
-                            lift
-                            (closest robot lambdas)))
-        seen            (if (= dest prev-dest)
-                          (conj visited robot)
-                          [robot])
-        ordered-moves   (concat (directions-from robot dest) [:U :D :L :R])
-        move-preferred? (fn [m]
-                          (let [future (step game-state m)]
-                            (and (not (some #{(:robot future)} seen))
-                                 (not (lose? future))
-                                 (move-allowed? game-state m))))
-        move-ok?        (fn [m]
-                          (let [future (step game-state m)]
-                            (and (not (lose? future))
-                                 (move-allowed? game-state m))))
-        preferred-moves (filter move-preferred? ordered-moves)
-        possible-moves  (filter move-ok? ordered-moves)
-        dir             (or (first preferred-moves)
-                            (first possible-moves)
-                            :A)
-        seen            (if (empty? preferred-moves)
-                          [robot]
-                          seen)
+(defn stupid-2-cost
+  [p {:keys [robot score rocks board moves] :as g} dest]
+  (let [dist       (manhattan-distance robot dest)
+        dead       (if (lose? g) 5000 0)
+        win        (if (win? g) -5000 0)
+        s          (- 0 score)
+        backtrack  (let [[m1 m2] (take 2 (reverse moves))]
+                     (if (= m1 (invert-move m2)) 10 0))
+        below-rock (let [[rx ry] robot
+                         above (get-in board [rx (inc ry)])]
+                     (if (= :* above)
+                       1000
+                       0))
+        rocks-moved (if (= (:rocks p) rocks)
+                      0
+                      1000)
         ]
-        (prn)
-        (prn "Move " (count moves))
-        (prn "Robot: " robot)
-        (prn "Seen: " seen)
-        (prn "Destination: " dest)
-        (prn "Distance: " (manhattan-distance robot dest))
-        (prn "Moves in order: " ordered-moves)
-        (prn "Preferred moves: " preferred-moves)
-        (prn "Possible moves: " possible-moves)
-        (prn "Moving: " dir)
-        (prn game-state)
-        (-> game-state
-            (step dir)
-            (assoc :visited seen)
-            (assoc :prev-dest dest))))
+    (+ dist dead win s backtrack below-rock)))
+
+(defn stupid-2
+  [{:keys [robot lambdas lift] :as g}]
+  (let [dest  (first (closest robot lambdas))
+        costs (for [m [:U :D :L :R]
+                    :when (move-allowed? g m)]
+                [(stupid-2-cost g (step g m) dest) m])
+        costs (sort costs)
+        dir   (second (first costs))
+        ]
+    (prn)
+    (prn costs)
+    (prn dir)
+    (prn robot)
+    (-> g
+        (step dir))))
 
 (defn run-sequence
   [game-state moves]
@@ -239,6 +224,7 @@
       (do
         (dosync
          (ref-set game-ref (f @game-ref)))
-        (if (< n 200)
-          (recur (inc n))))
+        (if (< n 1000)
+          (recur (inc n))
+          (prn "Terminating early")))
       @game-ref)))
