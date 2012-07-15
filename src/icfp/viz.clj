e7681d48a11e9bb89e47d269b66768347bb86831
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-07-15 13:54:48 -0600

    larger sized sprites; text overlay support

--- src/icfp/viz.clj
+++ src/icfp/viz.clj
@@ -2,20 +2,21 @@
   (:require [icfp.core :as core])
   (:use quil.core))
 
-(def sprite-size 16)
+(def sprite-size 24)
 
 (declare images)
 (declare colors)
 
 (defn draw-game
-  [{:keys [board current-goal] :as game}]
+  [{:keys [board current-goal overlays] :as game :or {overlays {}}}]
   (let [trans-x #(* sprite-size %)
         trans-y #(* sprite-size (- (inc (core/height board)) %))]
+
     (doseq [[x ys] board
             [y obj] ys
             :let [xp (trans-x x)
                   yp (trans-y y)
-                  overlay (cond
+                  outline (cond
                             (= [x y] current-goal)
                             :goal)]]
 
@@ -23,19 +24,27 @@
       (stroke 100 100 100)
 
       (when-let [i (images obj)]
-        (image i (- xp sprite-size) (- yp sprite-size)))
+        (image i (- xp sprite-size) (- yp sprite-size) sprite-size sprite-size))
 
       (when-let [c (colors obj)]
         (apply fill c)
-        (rect (- xp sprite-size) (- yp sprite-size) 16 16)
+        (rect (- xp sprite-size) (- yp sprite-size) sprite-size sprite-size)
         (no-fill))
 
-      (when-let [c (colors overlay)]
+      (when-let [c (colors outline)]
         (no-fill)
         (apply stroke c)
-        (stroke-weight 3)
-        (rect (- xp sprite-size) (- yp sprite-size) 14 14)
-        (no-stroke)))))
+        (stroke-weight 2)
+        (rect (- xp sprite-size) (- yp sprite-size) (- sprite-size 2) (- sprite-size 2))
+        (no-stroke))
+
+      (when-let [txt (overlays [x y])]
+        (fill 255 255 255)
+        (text-align :left :top)
+        (text-size 8)
+        (text (str txt) (- xp sprite-size) (- yp sprite-size))
+        (no-fill))
+      )))
 
 (defn setup-with-game
   [{:keys [board] :as game}]
