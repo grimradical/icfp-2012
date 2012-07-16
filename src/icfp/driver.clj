87f46f2ba14cca0bae2cae94a9e227f39bbf6550
Deepak Giridharagopal <deepak@puppetlabs.com>
2012-07-15 20:43:22 -0600

    contest submission packaging

--- src/icfp/driver.clj
+++ src/icfp/driver.clj
@@ -10,7 +10,8 @@
     (try
       (let [game-state (ai/run-ai ai/a*-ai game-ref)
             moves      (apply str (map #(str (name %)) (:moves @game-ref)))]
-        (print moves))
+        (println moves)
+        (flush))
       (catch Exception e
         (binding [*out* *err*]
           (.printStackTrace e))))))
